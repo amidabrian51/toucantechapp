db7192a5d34cc70d11f14944544503d7ee10fdde
Brian Patterson <briankaty1@me.com>
2017-05-25 20:10:10 +0100

    Initial Commit

---
+++ toucantechapp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  toucantechapp
+//
+//  Created by Katy Patterson on 25/05/2017.
+//  Copyright © 2017 Brian Patterson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
